9c312ff24a48ffabbd65553bb406a256c84ead73
Nhan Duong <nhan.duonghoang91@gmail.com>
2020-07-08 15:03:35 +0700

    verify token

--- index.js
+++ index.js
@@ -117,5 +117,7 @@ var unifiedServer = function (req, res) {
 
 var router = {
   'ping': handlers.ping,
-  'users': handlers.users
+  'users': handlers.users,
+  'tokens': handlers.tokens,
+  'checks': handlers.checks
 };
